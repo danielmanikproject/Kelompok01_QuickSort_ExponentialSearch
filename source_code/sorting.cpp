c3359a35e6dabbe0cb32f22502eb8fd2a04e44eb
chessapangaribuan89-eng <chessapangaribuan89@gmail.com>
2026-06-03 13:57:50 +0700

    Struktur untuk menyimpan hasil analisis sorting, Fungsi pembantu untuk menampilkan array (untuk fitur trace data kecil)

--- source_code/sorting.cpp
+++ source_code/sorting.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+
+// Struktur untuk menyimpan hasil analisis sorting
+struct SortResult {
+    double time_ms;
+    long long comparisons;
+    long long swaps;
+};
+
+// Fungsi pembantu untuk menampilkan array (untuk fitur trace data kecil)
+template <typename T>
+void printArray(T arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " | ";
+    }
+    std::cout << "\n";
+}
 
